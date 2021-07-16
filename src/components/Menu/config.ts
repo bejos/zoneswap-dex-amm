930e39d1a483a78349eaff16b10b2e91d2bf6ec6
Pancakeswap <pancakeswap@gmail.com>
2021-07-17 01:46:27 +0700

    init

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,92 +4,87 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'https://www.goosedefi.com/'
+    href: 'https://zoneswap.org',
   },
   {
     label: 'Trade',
     icon: 'TradeIcon',
-    initialOpenState: true,
     items: [
       {
-        label: 'Exchange',
-        href: '/swap'
+        label: 'Swap V2',
+        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x81e032c97a5cbbf06851412e2b1adcb2b51de9d1',
       },
       {
-        label: 'Liquidity',
-        href: '/pool'
-      }
-    ]
-  },
-  {
-    label: 'Farms',
-    icon: 'FarmIcon',
-    href: 'https://www.goosedefi.com/farms'
-  },
-  {
-    label: 'Nests',
-    icon: 'PoolIcon',
-    href: 'https://www.goosedefi.com/nests'
-  },
-  // {
-  //   label: 'Lottery',
-  //   icon: 'TicketIcon',
-  //   href: 'https://www.goosedefi.com/lottery'
-  // },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+        label: 'Swap OLD',
+        href: '/swap',
       },
       {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/goose-finance',
+        label: 'Liquidity V2',
+        href: 'https://exchange.pancakeswap.com/#/add/ETH/0x81e032c97a5cbbf06851412e2b1adcb2b51de9d1',
       },
       {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      },
-    ]
-  },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: "Github",
-        href: "https://github.com/goosedefi/",
-      },
-      {
-        label: "Docs",
-        href: "https://goosedefi.gitbook.io/goose-finance/",
-      },
-      {
-        label: "Blog",
-        href: "https://goosefinance.medium.com/",
+        label: 'Liquidity OLD',
+        href: '/pool',
       },
     ],
   },
   {
-    label: 'Partnerships/IFO',
-    icon: 'GooseIcon',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+    label: 'Farms ( SOON )',
+    icon: 'FarmIcon',
+    href: '/#',
   },
   {
-    label: 'Audit by Hacken',
-    icon: 'AuditIcon',
-    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+    label: 'Nests ( SOON )',
+    icon: 'PoolIcon',
+    href: '/#',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Listing',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'CoinGecko ( SOON )',
+        href: '#',
+      },
+      {
+        label: 'CoinMarketCap ( SOON )',
+        href: '#',
+      },
+    ],
   },
   {
-    label: 'Audit by CertiK',
+    label: 'Info',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Whitepaper',
+        href: 'https://zoneswap.org/files/zoneswap.pdf',
+      },
+      {
+        label: 'Github',
+        href: 'https://github.com/zoneswap/',
+      },
+    ],
+  },
+  {
+    label: 'Audit',
     icon: 'AuditIcon',
-    href: 'https://certik.org/projects/goose-finance',
+    href: 'https://certik.org/projects/cookiesswap',
   },
 ]
 
