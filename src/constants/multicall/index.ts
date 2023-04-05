8999053907544c3a0d76122bb2357aeae33a09d7
Mlbb Fan <77885074+retjehnetwork@users.noreply.github.com>
2023-04-06 02:57:51 +0700

    multical factory testnet

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -3,7 +3,7 @@ import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0x40fcd694c9ebbc46a2230ad498fbde11ae0111a8', // TODO
-  [ChainId.TESTNET]: '0x40fcd694c9ebbc46a2230ad498fbde11ae0111a8'
+  [ChainId.TESTNET]: '0xEe0df471Ea84a504755d157002C23275D0C05De8'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -5,7 +5,7 @@ import V1_FACTORY_ABI from './v1_factory.json'
 
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0xe0b8838e8d73ff1CA193E8cc2bC0Ebf7Cf86F620', // TODO
-  [ChainId.TESTNET]: '0xe0b8838e8d73ff1ca193e8cc2bc0ebf7cf86f620'
+  [ChainId.TESTNET]: '0x72791d81E872184dED1323fF85DFC46048c4795B'
 }
 
 const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
